0c218ccbecabbc2798cdbd6fc5cea365e17326b0
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2023-02-15 14:09:53 -0300

    atualizando

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+
+
+
+export default function ImagePickerExample() {
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  const [image, setImage] = useState(null);
+
+  const [segundos,setarSegundos] = useState(0);
+  const [minutos,setarMinutos] = useState(0);
+  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [alarmeSound, setarAlarmeSound] = useState([
+    {
+      selecionado: true,
+      som: 'alarme 1',
+      file: 'alarme1.mp3'
+
+    },
+    {
+      selecionado:false,
+      som: 'alarme 2',
+      file: 'alarme2.mp3'
+    },
+
+  ]);
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color:'white',fontSize:30}}>Selecione o seu tempo</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +58,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#54bf00',
     alignItems: 'center',
     justifyContent: 'center',
   },
